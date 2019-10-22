43704764a96c0917030382bece72bc4624de8ef8
raining_heavily <1194539573@qq.com>
2019-10-22 12:34:07 +0800

    修改相关依赖

--- src/components/view/test/test.vue
+++ src/components/view/test/test.vue
@@ -78,11 +78,6 @@ export default {
     //this.init();
   },
   methods: {
-    init() {
-      var showdown = require("showdown");
-      var converter = new showdown.Converter();
-      this.converter = converter;
-    },
     contentChanged() {
       var html = this.converter.makeHtml(this.content);
       document.getElementById("show-content").innerHTML = html;
